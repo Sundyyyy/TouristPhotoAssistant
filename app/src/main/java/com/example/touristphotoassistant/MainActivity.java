8c9712a287fb941df321d6ceaf36908eba75a3fd
Denis <9447668@gmail.com>
2022-02-19 02:11:25 +0300

    rework RV 1

--- app/src/main/java/com/example/touristphotoassistant/MainActivity.java
+++ app/src/main/java/com/example/touristphotoassistant/MainActivity.java
@@ -4,8 +4,12 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.touristphotoassistant.ui.helper.ApplicationSettings;
+import com.example.touristphotoassistant.ui.photocard.RecyclerTouchListener;
+import com.example.touristphotoassistant.ui.photocard.RecyclerviewAdapter;
+import com.example.touristphotoassistant.ui.photocard.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.touristphotoassistant.databinding.ActivityMainBinding;
 
@@ -28,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.INTERNET,
             Manifest.permission.CAMERA
     };
+
+    private RecyclerView recyclerView;
+    private RecyclerviewAdapter recyclerviewAdapter;
+    private RecyclerTouchListener touchListener;
 
     private ActivityMainBinding binding;
 
@@ -50,6 +59,56 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerviewAdapter = new RecyclerviewAdapter(this);
+
+        final List<Task> taskList = new ArrayList<>();
+        Task task = new Task("Buy Dress","Buy Dress at Shoppershop for coming functions");
+        taskList.add(task);
+        task = new Task("Go For Walk","Wake up 6AM go for walking");
+        taskList.add(task);
+        task = new Task("Office Work","Complete the office works on Time");
+        taskList.add(task);
+        task = new Task("watch Repair","Give watch to service center");
+        taskList.add(task);
+        task = new Task("Recharge Mobile","Recharge for 10$ to my **** number");
+        taskList.add(task);
+        task = new Task("Read book","Read android book completely");
+        taskList.add(task);
+
+        recyclerviewAdapter.setTaskList(taskList);
+        recyclerView.setAdapter(recyclerviewAdapter);
+
+        touchListener = new RecyclerTouchListener(this,recyclerView);
+        touchListener
+                .setClickable(new RecyclerTouchListener.OnRowClickListener() {
+                    @Override
+                    public void onRowClicked(int position) {
+                        Toast.makeText(getApplicationContext(),taskList.get(position).getName(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onIndependentViewClicked(int independentViewID, int position) {
+
+                    }
+                })
+                .setSwipeOptionViews(R.id.delete_task,R.id.edit_task)
+                .setSwipeable(R.id.rowFG, R.id.rowBG, new RecyclerTouchListener.OnSwipeOptionsClickListener() {
+                    @Override
+                    public void onSwipeOptionClicked(int viewID, int position) {
+                        switch (viewID){
+                            case R.id.delete_task:
+                                taskList.remove(position);
+                                recyclerviewAdapter.setTaskList(taskList);
+                                break;
+                            case R.id.edit_task:
+                                Toast.makeText(getApplicationContext(),"Edit Not Available",Toast.LENGTH_SHORT).show();
+                                break;
+
+                        }
+                    }
+                });
+        recyclerView.addOnItemTouchListener(touchListener);
 
     }
 
